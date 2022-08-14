cb44c9d2479c9b321f95823b0c5e12803d9819e6
kishan jangde <kishanknows@gmail.com>
2022-08-14 22:39:00 +0530

    Add files via upload

---
+++ B. Permutation Chain.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	    int t;
+	    cin>>t;
+	    for(int i=0;i<t;i++){
+	        int n;
+	        cin>>n;
+	        cout<<n<<endl;
+	        int arr[n];
+	        for(int j=0;j<n;j++){
+	            arr[j]=j+1;
+	        }
+	        for(int j=0;j<n;j++){
+	            for(int k=0;k<n;k++){
+	                cout<<arr[k]<<" ";
+	            }
+	            cout<<endl;
+	            int temp = arr[j];
+	            arr[j] = arr[j+1];
+	            arr[j+1] = temp;
+	        }
+	    }
+	return 0;
+}
